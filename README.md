13711a059ebadf4cd97d6eecd91acff2348e274c
NathanPB <nathan.pbombana@gmail.com>
2020-09-14 03:10:42 -0300

    Support to async

--- README.md
+++ README.md
@@ -2,6 +2,7 @@ Really simple and dependency-free lazy properties for Javascript
 
 ```js
 const lazy = require('lazyasfk')
+const lazyAsync = require('lazyasfk/async')
 
 const property = lazy(() => 'how could I be more lazy?')
 
@@ -13,6 +14,20 @@ property.forget()
 console.log(property.isPresent())   // > false
 property.forceCompute()
 console.log(property.isPresent())   // > true
+
+// Also async
+
+const expensiveAndTimeConsumingHttpRequestOrSomethingIdk = async () => 'foobar'
+
+const lazyProperty = lazyAsync(expensiveAndTimeConsumingHttpRequestOrSomethingIdk)
+console.log(lazyProperty.isPresent())   // > false
+console.log(lazyProperty.get())         // > obviously not foobar
+console.log(lazyProperty.isPresent())   // > true
+
+lazyProperty.forget()
+console.log(lazyProperty.isPresent())   // > false
+lazyProperty.forceCompute()
+console.log(lazyProperty.isPresent())   // > true
 ```
 
 The function names are pretty self-explanatory, I don't need I'll need to write function references for this.
--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,26 +1,56 @@
 const { expect } = require('chai')
 const { describe, it } = require('mocha')
 const lazy = require('../src')
+const lazyAsync = require('../src/async')
 
-describe('#get', () => {
-  it('should lazily load the value', () => {
-    const prop = lazy(() => 'foo')
-    expect(prop.isPresent()).to.equals(false)
-    expect(prop.get()).to.equals('foo')
-    expect(prop.isPresent()).to.equals(true)
+describe('lazyasfk', () => {
+  describe('blocking', () => {
+    describe('#get', () => {
+      it('should lazily load the value', () => {
+        const prop = lazy(() => 'foo')
+        expect(prop.isPresent()).to.equals(false)
+        expect(prop.get()).to.equals('foo')
+        expect(prop.isPresent()).to.equals(true)
+      })
+
+      it('should force compute the value', () => {
+        const prop = lazy(() => 'bar')
+        expect(prop.isPresent()).to.equals(false)
+        prop.forceCompute()
+        expect(prop.isPresent()).to.equals(true)
+      })
+
+      it('should forget the computed value', () => {
+        const prop = lazy(() => 'foobar')
+        prop.forceCompute()
+        prop.forget()
+        expect(prop.isPresent()).to.equals(false)
+      })
+    })
   })
 
-  it('should force compute the value', () => {
-    const prop = lazy(() => 'bar')
-    expect(prop.isPresent()).to.equals(false)
-    prop.forceCompute()
-    expect(prop.isPresent()).to.equals(true)
-  })
+  describe('async', () => {
+    describe('#get', () => {
+      it('should lazily async load the value', async () => {
+        const prop = lazyAsync(async () => 'foo')
+        expect(prop.isPresent()).to.equals(false)
+        expect(await prop.get()).to.equals('foo')
+        expect(prop.isPresent()).to.equals(true)
+      })
 
-  it('should forget the computed value', () => {
-    const prop = lazy(() => 'foobar')
-    prop.forceCompute()
-    prop.forget()
-    expect(prop.isPresent()).to.equals(false)
+      it('should async force compute the value', async () => {
+        const prop = lazyAsync(async () => 'bar')
+        expect(prop.isPresent()).to.equals(false)
+        await prop.forceCompute()
+        expect(prop.isPresent()).to.equals(true)
+      })
+
+      it('should forget the computed value', async () => {
+        const prop = lazyAsync(async () => 'foobar')
+        await prop.forceCompute()
+        prop.forget()
+        expect(prop.isPresent()).to.equals(false)
+      })
+    })
   })
 })
